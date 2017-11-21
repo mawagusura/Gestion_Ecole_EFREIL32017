b133b5597dcf69b82be77c8e7a050f432f2a124d
H0tmilk <chollet.kilian@gmail.com>
2017-11-21 08:44:40 +0100

    Fichiers IntelliJ supprimés

--- README.md
+++
@@ -1,2 +0,0 @@
-# Gestion_Ecole_EFREIL32017
-Software de gestion administrative et académique d'une école (collège/lycée)
--- View/LoginView.java
+++ View/LoginView.java
@@ -12,10 +12,6 @@ public class LoginView extends JPanel{
         this.frame = new JFrame("Login");
         frame.setSize (300, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-
-
         frame.setVisible(true);
     }
 }
