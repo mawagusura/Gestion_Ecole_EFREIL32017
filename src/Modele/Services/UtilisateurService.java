a8d72f991bd8294ca9640c20355be7904408698f
H0tmilk <chollet.kilian@gmail.com>
2017-11-22 19:40:58 +0100

    Ajout fonction de hachage mot de passe

--- src/Modele/Services/UtilisateurService.java
+++ src/Modele/Services/UtilisateurService.java
@@ -1,5 +1,8 @@
 package Modele.Services;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import Modele.DAO.UtilisateurDAO;
 import Modele.DBConnector;
 import Modele.Javabean.Utilisateur;
@@ -21,7 +24,29 @@ public class UtilisateurService {
     }
 
     public boolean verifyPassword(Utilisateur user, String password) {
-        return user.getHash_mdp().equals(password);
+        return user.getHash_mdp().equals(hash_password(password));
     }
 
+    private String hash_password(String password) {
+        // Il s'agit d'un algorithme de hachage SHA256
+
+        StringBuffer sb = new StringBuffer();
+        try {
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+
+            byte byteData[] = md.digest();
+
+            //convert the byte to hex format method 1
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+        } catch(NoSuchAlgorithmException e) {
+            System.err.println("Hash du mot de passe impossible.");
+        }
+
+        return sb.toString();
+    }
 }
