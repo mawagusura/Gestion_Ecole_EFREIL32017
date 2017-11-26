a369cdecf3cd5b105d2eba3b17ac10ca5ca51ce8
H0tmilk <chollet.kilian@gmail.com>
2017-11-26 14:05:08 +0100

    Rajout MatiereService

---
+++ src/Modele/Services/MatiereService.java
@@ -0,0 +1,28 @@
+package Modele.Services;
+
+import Modele.DAO.MatiereDAO;
+import Modele.DBConnector;
+import Modele.JavaBean.Eleve;
+import Modele.JavaBean.Matiere;
+
+import java.util.ArrayList;
+
+public class MatiereService {
+    private MatiereDAO dao;
+
+    public MatiereService() {
+        this.dao = new MatiereDAO(DBConnector.getInstance());
+    }
+
+    public Matiere getMatiere(int id) {
+        return dao.find(id);
+    }
+
+    public ArrayList<Matiere> getMatieres(Eleve e) {
+        return dao.findByEleve(e.getMatricule());
+    }
+
+    public ArrayList<Matiere> getAllMatieres() {
+        return dao.findAll();
+    }
+}
